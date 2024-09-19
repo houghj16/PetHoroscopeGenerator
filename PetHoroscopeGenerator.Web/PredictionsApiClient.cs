4c2cdc9b78d652c923c0003253dc8e6f29d787a9
Jessie Houghton <jehoughton@microsoft.com>
2024-09-19 11:38:59 -0700

    Added AI Text Generation

---
+++ PetHoroscopeGenerator.Web/PredictionsApiClient.cs
@@ -0,0 +1,68 @@
+namespace PetHoroscopeGenerator.Web;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using static System.Net.WebRequestMethods;
+
+public class PredictionsApiClient(HttpClient httpClient)
+{
+    public async Task<string?> GetPredictionAsync(string petDescription, string previewURL, int maxItems = 10, CancellationToken cancellationToken = default)
+    {
+
+        var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+        string endpoint = config["AZURE_OPENAI_ENDPOINT"];
+        string deployment = config["AZURE_OPENAI_GPT_NAME"];
+        string key = config["AZURE_OPENAI_KEY"];
+
+        // Create a Kernel containing the Azure OpenAI Chat Completion Service
+        Kernel kernel = Kernel.CreateBuilder()
+            .AddAzureOpenAIChatCompletion(deployment, endpoint, key)
+            .Build();
+
+        // Create and print out the prompt
+        string prompt = $"""
+            Please generate a horoscope for a pet based on the following information and image:
+            {petDescription}
+            """;
+        Console.WriteLine($"user >>> {prompt}");
+
+        // Create a ChatHistory object and add the system message
+        var chat = kernel.GetRequiredService<IChatCompletionService>();
+        var history = new ChatHistory();
+        history.AddSystemMessage("""
+            Do not use any markdown formatting, octothorpes, or asteriks. 
+            Instead add a newline after headers. 
+            Limit the output to 600 characters.
+            Use the provided image to say something specific about the dog.
+            Include a horoscope for the day and one line sections for a lucky treat, a favorite toy, a fun activity, and what to watch out for.
+            Format responses like this:
+            `[pet name] Pet Horoscope *emojis*
+
+            [horoscope]
+
+            Lucky Treat: [treat] *emojis*  
+            Favorite Toy: [toy] *emojis*
+            Fun Activity: [activity] *emojis*
+            Watch Out For: [danger] *emojis*`
+            Add emojis to make the tone playful.
+            """);
+
+        // Add the image and userMessage message to the ChatHistory
+        var imageContent = new ImageContent(previewURL);
+
+        var collectionItems = new ChatMessageContentItemCollection
+        {
+            new TextContent(prompt),
+            imageContent
+        };
+
+        history.AddUserMessage(collectionItems);
+
+        var result = await chat.GetChatMessageContentsAsync(history);
+        return result[^1].Content;
+
+        //return await kernel.InvokePromptAsync<string>(prompt, new(new OpenAIPromptExecutionSettings() { MaxTokens = 400 }), cancellationToken: cancellationToken);
+    }
+}
